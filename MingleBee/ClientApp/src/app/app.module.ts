d7411bc28b774cb56cd49d7e01e816674fdc09fa
Gayatrigurav <gayatri.gurav86@gmail.com>
2020-04-27 01:12:11 -0500

    Auth service injected in the Angular component

--- MingleBee/ClientApp/src/app/app.module.ts
+++ MingleBee/ClientApp/src/app/app.module.ts
@@ -5,32 +5,23 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    ValueComponent
+    AppComponent,    
+    ValueComponent, NavComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    RouterModule.forRoot([])
   ],
-  providers: [],
+  providers: [
+  AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
