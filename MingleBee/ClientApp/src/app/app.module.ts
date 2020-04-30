855c1dbf7b2c16eed0c291d270809d9218a356cd
Gayatrigurav <gayatri.gurav86@gmail.com>
2020-04-30 16:21:36 -0500

    Registration functionality added through angular UI

--- MingleBee/ClientApp/src/app/app.module.ts
+++ MingleBee/ClientApp/src/app/app.module.ts
@@ -5,14 +5,15 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,    
-    ValueComponent, NavComponent
+    NavComponent, HomeComponent, RegisterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -20,6 +21,12 @@ import { AuthService } from './_services/auth.service';
     FormsModule,
     RouterModule.forRoot([])
   ],
+
+
+
+
+
+
   providers: [
   AuthService],
   bootstrap: [AppComponent]
--- MingleBee/Controllers/ValuesController.cs
+++ MingleBee/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace MingleBee.Controllers
 
         }
         // GET: api/<controller>
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
